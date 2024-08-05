0c1ea14e8c81833d6bc41b1ec502c173246493f3
splashray <olayemi0427@gmail.com>
2024-08-05 14:46:50 +0100

    added port

--- server.py
+++ server.py
@@ -26,5 +26,6 @@ def send_command():
 
     return jsonify({"status": "Command sent successfully"}), 200
 
-if __name__ == "__main__":
-    app.run(port=5000)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
